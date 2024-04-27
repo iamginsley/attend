517bfea3d8765bf24eb14c40d3e27e029a83b0e0
Kevin Hetzenauer <kevin@hetzenauer.me>
2024-04-27 12:31:08 +0200

    fix: deploy

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,19 +1,20 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point of the Spring Boot application.
- *
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  *
  */
 @SpringBootApplication
 @Theme(value = "attend-vaadin")
+@PWA(name = "Attend Vaadin", shortName = "Attend Vaadin")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
