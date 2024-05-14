b27dc123430918892db3f77becdca0a93093fb1a
iamginsley <99986139+iamginsley@users.noreply.github.com>
2024-05-14 23:58:58 +0200

    optimize imports

--- src/main/java/com/example/application/views/admin/AdminView.java
+++ src/main/java/com/example/application/views/admin/AdminView.java
@@ -12,9 +12,8 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
-import org.springframework.security.access.prepost.PreAuthorize;
 
-@PreAuthorize("hasAuthority('admin')")
+@PermitAll
 @PageTitle("Admin | Vaadin CRM")
 @Route(value = "admin-view", layout = MainLayout.class)
 public class AdminView extends VerticalLayout {
