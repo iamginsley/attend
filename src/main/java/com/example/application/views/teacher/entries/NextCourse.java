730260d781a2f2c1bdeb6f3eaa5f3a6b60f81e02
Kevin Hetzenauer <kevin@hetzenauer.me>
2024-05-15 19:23:24 +0200

    feat: show lecturer courses

--- src/main/java/com/example/application/views/teacher/entries/NextCourse.java
+++ src/main/java/com/example/application/views/teacher/entries/NextCourse.java
@@ -4,11 +4,12 @@ import com.example.application.views.abstracts.UserViewEntry;
 import com.example.application.views.components.CustomButton;
 
 public class NextCourse extends UserViewEntry {
-    public NextCourse() {
+
+    public NextCourse(CustomButton[] buttons) {
         super("Next Course");
         this.addFirstRow(new String[]{"Fachhochschule Hagenberg", "07.05.2024"});
         this.addSecondRow(new String[]{"IT-Strategie","08:30"});
         this.addThirdRow(new String[]{"FHHA | Raum 202"});
-        this.addButtonLayout(new CustomButton[] {new CustomButton("View all")});
+        this.addButtonLayout(buttons);
     }
 }
