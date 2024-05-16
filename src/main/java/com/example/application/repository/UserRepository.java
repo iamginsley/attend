842bb07f3e6d7e0adedd21634af1a70a503b2ede
iamginsley <dominik.gintsberger@outlook.de>
2024-05-16 19:21:21 +0200

    added UserService and implemented - getUserId
    fix: changed UserRepository value to integer

--- src/main/java/com/example/application/repository/UserRepository.java
+++ src/main/java/com/example/application/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.application.repository;
 import com.example.application.data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
     User findByEmail(String email);
 }
